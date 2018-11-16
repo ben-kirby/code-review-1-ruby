46d89e204f62053a1364c7831740c4c67481e41c
Ben Kirby <benkirby@Bens-MacBook-Pro.local>
2018-11-16 11:12:45 -0800

    Working on getting it to detect vowels in words

--- lib/review1.rb
+++ lib/review1.rb
@@ -4,27 +4,28 @@ class Anagram
     @word2 = word2
   end
 
-  def checker
+  def anagram
     array1 = []
     array2 = []
 
     array1 = @word1.split("")
     array2 = @word2.split("")
 
-    #check for vowels
+    if (array1.detect{ |e| e == "a" || e == "e" || e == "i" || e == "o" || e == "u" || e == "y"})
+      array1.reject!{ |e| e == " " }
+      array2.reject!{ |e| e == " " }
 
-    array1.reject!{ |e| e == " " }
-    array2.reject!{ |e| e == " " }
+      array1.map!{ |e| e.upcase }
+      array2.map!{ |e| e.upcase }
 
-    array1.map!{ |e| e.upcase }
-    array2.map!{ |e| e.upcase }
-
-    array1.sort!{ |a, b| a <=> b }
-    array2.sort!{ |a, b| a <=> b }
-    if array1 == array2
-      true
-    else
-      false
+      array1.sort!{ |a, b| a <=> b }
+      array2.sort!{ |a, b| a <=> b }
+      if array1 == array2
+        true
+      else
+        false
+      end
     end
+
   end
 end
