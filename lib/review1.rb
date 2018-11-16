3cf8448d7228e0458a548257d6fc7467b4ad9350
Ben Kirby <benkirby@Bens-MacBook-Pro.local>
2018-11-16 13:21:58 -0800

    This commit will now have development for antigrams.

--- lib/review1.rb
+++ lib/review1.rb
@@ -5,33 +5,28 @@ class Anagram
   end
 
   def anagram
-    array1 = []
-    array2 = []
-
 #remove all non-alpha characters
     anagramWord1 = @userWord1.gsub(/[^a-zA-Z]/, '')
     anagramWord2 = @userword2.gsub(/[^a-zA-Z]/, '')
 
 #convert to array
-    array1 = anagramWord1.split("")
-    array2 = anagramWord2.split("")
+    wordArray1 = anagramWord1.split("")
+    wordArray2 = anagramWord2.split("")
 
 #check for vowels
-    if (array1.detect{ |e| e == 'a' || e == 'e' || e == 'i' || e == 'o' || e == 'u' || e == 'y'}) &&
-       (array2.detect{ |e| e == 'a' || e == 'e' || e == 'i' || e == 'o' || e == 'u' || e == 'y'})
-
-     #get rid of spaces
+    if (wordArray1.detect{ |e| e == 'a' || e == 'e' || e == 'i' || e == 'o' || e == 'u' || e == 'y'}) &&
+       (wordArray2.detect{ |e| e == 'a' || e == 'e' || e == 'i' || e == 'o' || e == 'u' || e == 'y'})
 
     #change all to upper case
-      array1.map!{ |e| e.upcase }
-      array2.map!{ |e| e.upcase }
+      wordArray1.map!{ |e| e.upcase }
+      wordArray2.map!{ |e| e.upcase }
 
     #sort alphabetically
-      array1.sort!{ |a, b| a <=> b }
-      array2.sort!{ |a, b| a <=> b }
+      wordArray1.sort!{ |a, b| a <=> b }
+      wordArray2.sort!{ |a, b| a <=> b }
 
     #check if theyre equal
-      if array1 == array2
+      if wordArray1 == wordArray2
         true
       else
         false
@@ -39,5 +34,9 @@ class Anagram
     else
       "FAKE WORDS. Get some vowels. Pathetic. I have great vowels. People love my vowels."
     end
+  end
+
+  def antigram
+    
   end
 end
--- spec/review1_spec.rb
+++ spec/review1_spec.rb
@@ -8,48 +8,58 @@ describe ('the class') do
     expect(word.anagram()).to(eq(true))
     end
 
-    it('Checks if an input letter "b" is the different than "a" and returns a bool') do
-        word = Anagram.new('a', 'b')
-      expect(word.anagram()).to(eq("FAKE WORDS. Get some vowels. Pathetic. I have great vowels. People love my vowels."))
-      end
+  it('Checks if an input letter "b" is the different than "a" and returns a bool') do
+      word = Anagram.new('a', 'b')
+    expect(word.anagram()).to(eq("FAKE WORDS. Get some vowels. Pathetic. I have great vowels. People love my vowels."))
+    end
 
-    it('Checks if a single word passed through twice are the same') do
-        word = Anagram.new('dog', 'dog')
-      expect(word.anagram()).to(eq(true))
-      end
+  it('Checks if a single word passed through twice are the same') do
+      word = Anagram.new('dog', 'dog')
+    expect(word.anagram()).to(eq(true))
+    end
 
-    it('Checks if two single word non-anagrams are different') do
-        word = Anagram.new('dog', 'cat')
-      expect(word.anagram()).to(eq(false))
-      end
+  it('Checks if two single word non-anagrams are different') do
+      word = Anagram.new('dog', 'cat')
+    expect(word.anagram()).to(eq(false))
+    end
 
-    it('Checks if two single word anagrams are different') do
-        word = Anagram.new('dog', 'god')
-      expect(word.anagram()).to(eq(true))
-      end
+  it('Checks if two single word anagrams are different') do
+      word = Anagram.new('dog', 'god')
+    expect(word.anagram()).to(eq(true))
+    end
 
-    it('Checks if two single word anagrams are the same with dufferent upper case letters') do
-        word = Anagram.new('Dog', 'God')
-      expect(word.anagram()).to(eq(true))
-      end
+  it('Checks if two single word anagrams are the same with dufferent upper case letters') do
+      word = Anagram.new('Dog', 'God')
+    expect(word.anagram()).to(eq(true))
+    end
 
-    it('Checks if two single word non-anagrams are the same with dufferent upper case letters') do
-        word = Anagram.new('Dog', 'Cat')
-      expect(word.anagram()).to(eq(false))
-      end
+  it('Checks if two single word non-anagrams are the same with dufferent upper case letters') do
+      word = Anagram.new('Dog', 'Cat')
+    expect(word.anagram()).to(eq(false))
+    end
 
-    it('Checks if a single-word and multi-word are anagrams (different upper case letters)') do
-        word = Anagram.new('Dormitory', 'dirty room')
-      expect(word.anagram()).to(eq(true))
-      end
+  it('Checks if a single-word and multi-word are anagrams (different upper case letters)') do
+      word = Anagram.new('Dormitory', 'dirty room')
+    expect(word.anagram()).to(eq(true))
+    end
 
-    it('Checks if a single-word and multi-word are anagrams (different upper case letters)') do
-        word = Anagram.new('Dormitory', 'dirty apartment')
-      expect(word.anagram()).to(eq(false))
-      end
+  it('Checks if a single-word and multi-word are anagrams (different upper case letters)') do
+      word = Anagram.new('Dormitory', 'dirty apartment')
+    expect(word.anagram()).to(eq(false))
+    end
 
-     it('Checks if both words have vowels or a y)') do
-          word = Anagram.new('Zxv', 'vxz')
-        expect(word.anagram()).to(eq("FAKE WORDS. Get some vowels. Pathetic. I have great vowels. People love my vowels."))
-        end
+ it('Checks if both words have vowels or a y)') do
+      word = Anagram.new('Zxv', 'vxz')
+    expect(word.anagram()).to(eq("FAKE WORDS. Get some vowels. Pathetic. I have great vowels. People love my vowels."))
+    end
+
+  it('Removes all non-alpha characters') do
+       word = Anagram.new('d123948-1834-$%$@^#&#$#$o%$^#%54637g', 'dog')
+     expect(word.anagram()).to(eq(true))
+     end
+
+ it('Detects an antigram') do
+      word = Anagram.new('abc', 'def')
+    expect(word.antigram()).to(eq(true))
+    end
   end
