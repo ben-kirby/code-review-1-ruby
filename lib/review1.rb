27e8f58cbcf0375b7c7475a8303ff0d817c5e61b
Ben Kirby <benkirby@Bens-MacBook-Pro.local>
2018-11-16 12:14:36 -0800

    It will now filter out all non-alpha characters from the two words. Also changed some naming conventions on the variables for clarity moving forward.

--- lib/review1.rb
+++ lib/review1.rb
@@ -1,23 +1,26 @@
 class Anagram
   def initialize(word1, word2)
-    @word1 = word1
-    @word2 = word2
+    @userWord1 = word1
+    @userword2 = word2
   end
 
   def anagram
     array1 = []
     array2 = []
 
+#remove all non-alpha characters
+    anagramWord1 = @userWord1.gsub(/[^a-zA-Z]/, '')
+    anagramWord2 = @userword2.gsub(/[^a-zA-Z]/, '')
+
 #convert to array
-    array1 = @word1.split("")
-    array2 = @word2.split("")
+    array1 = anagramWord1.split("")
+    array2 = anagramWord2.split("")
 
 #check for vowels
     if (array1.detect{ |e| e == 'a' || e == 'e' || e == 'i' || e == 'o' || e == 'u' || e == 'y'}) &&
        (array2.detect{ |e| e == 'a' || e == 'e' || e == 'i' || e == 'o' || e == 'u' || e == 'y'})
+
      #get rid of spaces
-      array1.reject!{ |e| e == " " }
-      array2.reject!{ |e| e == " " }
 
     #change all to upper case
       array1.map!{ |e| e.upcase }
